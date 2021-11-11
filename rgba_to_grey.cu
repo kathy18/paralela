0e4bb6076e0fbec71790a5ee05906b20b35f3c1e
windark <katherine.jimenez@ucsp.edu.pe>
2021-11-11 16:18:44 -0500

    Update rgba_to_grey.cu

--- rgba_to_grey.cu
+++ rgba_to_grey.cu
@@ -10,12 +10,7 @@ void rgba_to_grey_kernel(const uchar4* const rgbaImage,
                        unsigned char* const greyImage,
                        int numRows, int numCols)
 {
-  // The mapping from components of a uchar4 to RGBA is:
-  // .x -> R ; .y -> G ; .z -> B ; .w -> A
-  //
-  //The output (greyImage) at each pixel should be the result of
-  //applying the formula: output = .299f * R + .587f * G + .114f * B;
-  //Note: We will be ignoring the alpha channel for this conversion
+  
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   uchar4 px = rgbaImage[i]; // thread pixel to process
   greyImage[i] = .299f * px.x +
@@ -27,9 +22,7 @@ void rgba_to_grey(uchar4 * const d_rgbaImage,
                   unsigned char* const d_greyImage, size_t numRows, size_t numCols)
 {
 
-  // Since it does not matter the relative position of a pixel
-  // the block - grid assign strategy will simply be to cover
-  // all pixels secuencially in 'x' axis
+  
   long long int total_px = numRows * numCols;  // total pixels
   long int grids_n = ceil(total_px / TxB); // grids numer
   const dim3 blockSize(TxB, 1, 1);
