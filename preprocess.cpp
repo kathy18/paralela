86058c7a7f44e6a8920eaac0edc725de540fc81b
windark <katherine.jimenez@ucsp.edu.pe>
2021-11-11 16:28:59 -0500

    Update preprocess.cpp

--- preprocess.cpp
+++ preprocess.cpp
@@ -15,15 +15,11 @@ unsigned char *d_greyImage__;
 size_t numRows() { return imageRGBA.rows; }
 size_t numCols() { return imageRGBA.cols; }
 
-//return types are void since any internal error will be handled by quitting
-//no point in returning error codes...
-//returns a pointer to an RGBA version of the input image
-//and a pointer to the single channel grey-scale output
-//on both the host and device
+
 void preProcess(uchar4 **inputImage, unsigned char **greyImage,
                 uchar4 **d_rgbaImage, unsigned char **d_greyImage,
                 const std::string &filename) {
-  //make sure the context initializes ok
+ 
   checkCudaErrors(cudaFree(0));
 
   cv::Mat image;
@@ -35,11 +31,10 @@ void preProcess(uchar4 **inputImage, unsigned char **greyImage,
 
   cv::cvtColor(image, imageRGBA, CV_BGR2RGBA);
 
-  //allocate memory for the output
+ 
   imageGrey.create(image.rows, image.cols, CV_8UC1);
 
-  //This shouldn't ever happen given the way the images are created
-  //at least based upon my limited understanding of OpenCV, but better to check
+ 
   if (!imageRGBA.isContinuous() || !imageGrey.isContinuous()) {
     std::cerr << "Images aren't continuous!! Exiting." << std::endl;
     exit(1);
@@ -49,12 +44,12 @@ void preProcess(uchar4 **inputImage, unsigned char **greyImage,
   *greyImage  = imageGrey.ptr<unsigned char>(0);
 
   const size_t numPixels = numRows() * numCols();
-  //allocate memory on the device for both input and output
+ 
   checkCudaErrors(cudaMalloc(d_rgbaImage, sizeof(uchar4) * numPixels));
   checkCudaErrors(cudaMalloc(d_greyImage, sizeof(unsigned char) * numPixels));
   checkCudaErrors(cudaMemset(*d_greyImage, 0, numPixels * sizeof(unsigned char))); //make sure no memory is left laying around
 
-  //copy input array to the GPU
+  
   checkCudaErrors(cudaMemcpy(*d_rgbaImage, *inputImage, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
 
   d_rgbaImage__ = *d_rgbaImage;
